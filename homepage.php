de4e03e72a014a7c4cde0a30294f155d343c632e
unknown <aaron1-cmd>
2025-08-20 01:15:55 -0700

    heading

---
+++ homepage.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>TYLON STUDIES</title>
+    <link rel="stylesheet" href="RWD.css">
+</head>
+<body>
+    <div class="main">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">TYLON STUDIES</h2>
+                <div class="menu">
+                    <ul>
+                        <li><a href="#" onclick="">HOME</a></li>
+                        <li><a href="#" onclick="">ABOUT</a></li>
+                        <li><a href="#" onclick="asd()">SERVICE</a></li>
+                        <li><a href="#" onclick="happy()">CONTACT</a></li>
+                    </ul>
+                </div>
+                <div class="search">
+                
+                </div>
+
+            </div>
+            <div class="content">
+                <h1 style="margin-left: 20%; margin-bottom: 20%;"><br><span></span><br></h1>
+                <p class="par"><h1>WELCOME TO <br><span>TYLON</span><br>STUDIES</h1></p>
+                <button class="cn" style="width: 160px; height: 40px; background: #ff7200; border: none; margin-bottom: 0px; margin-left: 20px; font-size: 18px; border-radius: 10px; cursor: pointer; color: aliceblue; transition: 0.3s ease-in-out;" onclick="demo()">Join as admin</button>
+            </div>
+            <div class="form" style="margin: 20%; background-color: rgba(0,0,0,0.8); margin-top: 13%; margin-left: 15%;">
+                <h2 style="color: aliceblue;">welcome</h2>
+                <input type="username" name="username" placeholder="username">
+                <button class="btnn" style="margin-left: 10px;margin-top: 10px; color: aliceblue; " onclick=""><a href="nnn.html">Start</a></button>
+                <p class="link" style="color: aliceblue; margin-left: 10px;">Start now for your future</p>
+                <p class="liw" style="color: aliceblue; margin-left: 10px;">follow as on</p>
+
+                    <div class="icon" style="margin-right: 50%;">
+                        <br>
+                       <a href="https://www.facebook.com/nishimweaaron/"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12.001 2.002c-5.522 0-9.999 4.477-9.999 9.999 0 4.99 3.656 9.126 8.437 9.879v-6.988h-2.54v-2.891h2.54V9.798c0-2.508 1.493-3.891 3.776-3.891 1.094 0 2.24.195 2.24.195v2.459h-1.264c-1.24 0-1.628.772-1.628 1.563v1.875h2.771l-.443 2.891h-2.328v6.988C18.344 21.129 22 16.992 22 12.001c0-5.522-4.477-9.999-9.999-9.999z"/></svg></a> 
+                       <a href="https://www.instagram.com/nishi_aaron4/"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.947 8.305a6.53 6.53 0 0 0-.419-2.216 4.61 4.61 0 0 0-2.633-2.633 6.606 6.606 0 0 0-2.186-.42c-.962-.043-1.267-.055-3.709-.055s-2.755 0-3.71.055a6.606 6.606 0 0 0-2.185.42 4.607 4.607 0 0 0-2.633 2.633 6.554 6.554 0 0 0-.419 2.185c-.043.963-.056 1.268-.056 3.71s0 2.754.056 3.71c.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.043 1.268.056 3.71.056s2.755 0 3.71-.056a6.59 6.59 0 0 0 2.186-.419 4.615 4.615 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.187.043-.962.056-1.267.056-3.71-.002-2.442-.002-2.752-.058-3.709zm-8.953 8.297c-2.554 0-4.623-2.069-4.623-4.623s2.069-4.623 4.623-4.623a4.623 4.623 0 0 1 0 9.246zm4.807-8.339a1.077 1.077 0 0 1-1.078-1.078 1.077 1.077 0 1 1 2.155 0c0 .596-.482 1.078-1.077 1.078z"/><circle cx="11.994" cy="11.979" r="3.003"/></svg></a>
+                       <a href="https://www.tiktok.com/aaron40548/"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"/></svg></a>
+                       <a href="https://www.youtube.com/cedro stin/"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21.593 7.203a2.506 2.506 0 0 0-1.762-1.766C18.265 5.007 12 5 12 5s-6.264-.007-7.831.404a2.56 2.56 0 0 0-1.766 1.778c-.413 1.566-.417 4.814-.417 4.814s-.004 3.264.406 4.814c.23.857.905 1.534 1.763 1.765 1.582.43 7.83.437 7.83.437s6.265.007 7.831-.403a2.515 2.515 0 0 0 1.767-1.763c.414-1.565.417-4.812.417-4.812s.02-3.265-.407-4.831zM9.996 15.005l.005-6 5.207 3.005-5.212 2.995z"/></svg></a>
+                    </div>
+
+            </div>
+        </div>
+    </div>
+    <script>
+        function demo(){
+             let x= prompt("enter the password." );
+            if (x=='nishi4'){
+                document.write("<H1>welcome mr.aaron</H1> <button>countinue</button>");
+            }else{
+                document.write("<h1>wrong password you are not the admin</h1>");
+            }
+        }
+       function happy(){
+        document.write("<center><h3>Names:Tylon studies<br>Contact:+250 796024823<br>Location:Kigali,Rwanda<br>instgram:tylon_trade1<br>facebook:tylontrade<br>email:tylontrade1@gmail.com</h3></center>");
+       }
+       function asd(){
+        document.write("<h3><center>Tylon trade we are here to develope your talents and buliding your future by providing the following services:<br>teaching you and training you about your career or tarent <br>helping you to choose your career<br>providing your wishes</center></h3>");
+       }
+    </script>
+   
+
+</body>
+</html>
